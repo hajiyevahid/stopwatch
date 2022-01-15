2c746b0806448728f01eec269bb36df71afd6fb3
vahidhajiyev <72050708+hajiyevahid@users.noreply.github.com>
2022-01-15 20:11:42 +0200

    Add files via upload

---
+++ exercises_advanced/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace exercises_advanced
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new StopWatch();
+            var input = "";
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "Start" && stopwatch.State() == false)
+                {
+
+                   
+                    stopwatch.Start();
+                    Console.WriteLine();
+                    Console.WriteLine("******************************************************");
+                }
+                else if (input == "Stop")
+                {
+
+                    stopwatch.Stop();
+                    Console.WriteLine("Stopped");
+                }
+
+
+                else if(input == "Start" && stopwatch.State() == true)
+                {
+                    Console.WriteLine("Please stop the timer for starting again !");
+                }
+
+            }                  
+        }
+    }
+}
---
+++ exercises_advanced/StopWatch.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Globalization;
+namespace exercises_advanced
+
+{
+    public class StopWatch
+    {
+        public  DateTime Current ;
+        public  DateTime Current1 ;
+        public TimeSpan interval;
+        public bool f = false;
+        
+        public DateTime giveMeTime()
+        {
+            return DateTime.Now;
+        }
+        public Boolean State()
+        {
+            return this.f;
+        }
+        public void Start()
+        {
+            this.f = true;
+            Current = this.giveMeTime();
+            Console.Write("Current start time is : " + Current );
+        }
+
+
+        public void Stop()
+        {
+            this.f = false;
+            Current1 = this.giveMeTime();
+            interval = Current1 - Current;
+            Console.WriteLine("Current end time is :" + Current1);
+            Console.WriteLine("Stopwatch time duration = " + interval);
+        }
+
+    }
+}
